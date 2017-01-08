3af8c933023d08f870d751529375a332340d196d
Elle Vargas <vargas.elle@gmail.com>
2017-01-07 19:27:30 -0800

    okay, so edit is not working right but at least delete and new are even though there is no button for new rn

--- app/controllers/adies_controller.rb
+++ app/controllers/adies_controller.rb
@@ -1,21 +1,31 @@
 class AdiesController < ApplicationController
   before_action :require_authentication!
+  before_action :find_adie, only: [:show, :edit]
 
   def index
     @adies = Adie.all
   end
 
-  def show
-    @adie = Adie.find(params[:id])
-  end
+  def show; end
 
   def new
     @adie = Adie.new
   end
 
   def create
-    upload
-    @Adies = Adie.last.first_name
+    # upload
+
+    @adie = Adie.new(first_name: params[:adie][:first_name],
+    last_name: params[:adie][:last_name],
+    cohort: params[:adie][:cohort],
+    email: params[:adie][:email],
+    pref_pronouns: params[:adie][:pref_pronouns],
+    twitter_handle: params[:adie][:twitter_handle],
+    linkedin: params[:adie][:linkedin],
+    github_username: params[:adie][:github_username],
+    internship_company: params[:adie][:internship_company],
+    current_company: params[:adie][:current_company])
+    @adie.save
 
     redirect_to adies_path
   end
@@ -24,12 +34,53 @@ class AdiesController < ApplicationController
   end
 
   def update
+    # if @adie.update(adie_params)
+    #   redirect_to request.referrer
+    # else
+    #   render :edit
+    # end
+
+    @adie = Adie.update(first_name: params[:adie][:first_name],
+    last_name: params[:adie][:last_name],
+    cohort: params[:adie][:cohort],
+    email: params[:adie][:email],
+    pref_pronouns: params[:adie][:pref_pronouns],
+    twitter_handle: params[:adie][:twitter_handle],
+    linkedin: params[:adie][:linkedin],
+    github_username: params[:adie][:github_username],
+    internship_company: params[:adie][:internship_company],
+    current_company: params[:adie][:current_company])
+
+    redirect_to adies_path # temporary fix
   end
 
   def upload
-    Adie.import(params[:file])
+    # @imported = Adie.import(params[:file])
+    # @imported.save
+    #
+    # redirect_to adies_path
   end
 
   def destroy
+    Adie.destroy(params[:id])
+
+    redirect_to adies_path
   end
+
+private
+
+  def adie_params
+    params.require(:adie).permit(:first_name, :last_name, :cohort, :email, :pref_pronouns, :twitter_handle, :linkedin, :github_username, :internship_company, :current_company)
+    # this is for update/create/new, SHOULD USE when passing a hash to any of those methods, do this to prevent injection risks
+  end
+
+  def find_adie
+    @adie = Adie.find(params[:id])
+    # if !@adie || !@user.tasks.include?(@mytask)
+    #   flash[:notice] = "Sorry, that is not one of your tasks."
+    #   redirect_to root_path
+    # end
+  end
+
+
 end
--- app/models/adie.rb
+++ app/models/adie.rb
@@ -1,12 +1,16 @@
 class Adie < ApplicationRecord
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :cohort, presence: true
+
   require 'csv'
 
-  def self.import(file)
-    CSV.foreach(file.path, headers: false) do |csv_obj|
-      # binding.pry
-      Adie.create!(cohort: csv_obj[1], pref_pronouns: csv_obj[2], first_name: csv_obj[3], last_name: csv_obj[4], email: csv_obj[5], twitter_handle: csv_obj[12], linkedin: csv_obj[13], github_username: csv_obj[14])
-    end
-  end
+  # def self.import(file)
+  #   CSV.foreach(file.path, headers: false) do |csv_obj|
+  #     # binding.pry
+  #     Adie.create!(cohort: csv_obj[1], pref_pronouns: csv_obj[2], first_name: csv_obj[3], last_name: csv_obj[4], email: csv_obj[5], twitter_handle: csv_obj[12], linkedin: csv_obj[13], github_username: csv_obj[14])
+  #   end
+  # end
 
 end
